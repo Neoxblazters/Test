785df396803207c02895a8418022a7d0671af45a
miner <minermu94@gmail.com>
2021-11-01 20:36:02 +0400

    Added logs

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import websocket, json, pprint, numpy, talib, datetime
+import websocket, json, pprint, numpy, talib, datetime, sys
 
 from binance.client import Client
 
@@ -6,22 +6,18 @@ from binance.enums import *
 
 SOCKET = "wss://stream.binance.com:9443/ws/shibusdt@kline_1m"
 
-PERIOD = 7
+PERIOD = 2
 BUY_SIG = 40
 SELL_SIG = 70
 TRADE_SYM = 'SHIBUSDT'
 
 closes = []
-in_position = True
 
-def now():
-    try:
-        now = datetime.datetime.now()
-        print(now.strftime("%Y-%m-%d %H:%M:%S"))
-    except Exception as e:
-        print("An error occured")
-        return False
-    return True
+old_stdout = sys.stdout
+log_sell = open("sell_signals.log")
+log_buy = open("buy_signals.log")
+log_closes = open("closes.log")
+log_rsi = open("rsi.log")
 
 def on_close(ws):
     print('Monitoring stopped')
@@ -30,40 +26,33 @@ def on_open(ws):
     print('Monitoring started')
 
 def on_message(ws, message):
-    #global closes
     json_message = json.loads(message)
     candle = json_message['k']
     is_candle_closed = candle['x']
     close = candle['c']
 
     if is_candle_closed:
-        print("Closing value {}".format(close))
         closes.append(float(close))
-        print("closes")
+        print(closes, file=open("closes.log", "a"))
 
     if len(closes) > PERIOD:
         np_closes = numpy.array(closes)
         RSI = talib.RSI(np_closes, PERIOD)
         latest_RSI = RSI[-1]
-        print("The current RSI is {}".format(lastest_RSI))
+        f = open('closes.log', 'r+')
+        f.truncate(0)
+        print(closes, file=open("closes.log", "a"))
+        print(latest_RSI, file=open("rsi.log", "a"))
 
     if latest_RSI > SELL_SIG:
-        print("Sell signal received")
-        if in_position:
-            print("Advised to sell at : ")
-            if now:
-                in_position = False
-        else:
-            print("Not in position to sell")
+        now = datetime.datetime.now()
+        print("Sell signal received", file=open("sell_signals.log"))
+        print(now.strftime("%Y-%m-%d %H:%M:%S", file=open("sell_signals.log")))
 
     if latest_RSI < BUY_SIG:
-        print("Buy signal received")
-        if in_position:
-            print("Already in position")
-        else:
-            print("Advised to buy at : ")
-            if now:
-                in_position = True
-
+        now = datetime.datetime.now()
+        print("Buy signal received", file=open("buy_signals.log"))
+        print(now.strftime("%Y-%m-%d %H:%M:%S", file=open("buy_signals.log")))
+        
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
